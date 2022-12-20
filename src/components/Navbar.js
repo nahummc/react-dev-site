d03ce6c6b6eaf5cbf5a9e1e592a9069e3d45f1be
nahummc <nahumcollicott@gmail.com>
2022-12-20 17:22:14 -0400

    changed navigation and added firebase deps

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,14 +12,15 @@ function BasicNav() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
             {/* <Nav.Link href="#link">About</Nav.Link> */}
             <NavDropdown title="More" id="basic-nav-dropdown">
-              <NavDropdown.Item href="contact">Contact me</NavDropdown.Item>
+              {/* <NavDropdown.Item href="contact">Contact me</NavDropdown.Item> */}
               {/* <NavDropdown.Item href="weather">
                 Weather
               </NavDropdown.Item> */}
               {/* <NavDropdown.Item href="#action/3.3">Chat</NavDropdown.Item> */}
-              <NavDropdown.Divider />
+              {/* <NavDropdown.Divider /> */}
               <NavDropdown.Item href="https://github.com/nahummc" target="_blank">
                 Github
               </NavDropdown.Item>
