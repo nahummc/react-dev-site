ae7238bcd556af1a9321a695fda22b8bce1e0e9c
nahummc <nahumcollicott@gmail.com>
2023-01-12 14:49:40 -0400

    edited footer to have some social links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import './Footer.css';
+import { SocialIcon } from 'react-social-icons';
+
 
 function Footer() {
   return (
     <div className='footer'>
         {/* <h2>Footer</h2> */}
             <h5>
-              <a href='/contact' className='footer-link'>Contact me</a>
+              <a href='/contact' className='footer-link'>Reach out!</a>
             </h5>
-            <br/>
 
-        <p>The form submission is a work in progress, so in the mean time, use <a href="mailto:nahum.development@gmail.com">this link</a> or the email below to get ahold of me!</p>
-        <p>nahum.development@gmail.com</p>
+        <p><a href="mailto:nahum.development@gmail.com">nahum.development@gmail.com</a></p>
+        <div className='socials'>
+          <SocialIcon className='social-btn grow' url="https://www.linkedin.com/in/nahum-collicott-594269200/"/>
+          <SocialIcon className='social-btn grow' url="https://twitter.com/NCollicott" />
+          <SocialIcon className='social-btn grow' url="https://github.com/nahummc" />
+          
+
+
+        </div>
+
+       
+        
     </div>
   )
 }
