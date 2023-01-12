b24a749f39fa71c6bc6e2890141d14b696a1731e
nahummc <nahumcollicott@gmail.com>
2023-01-12 15:17:20 -0400

    deleted a stray character

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -73,7 +73,7 @@ function Calculator() {
 // onClick={handleClick()}
   return (
       <div class="calc-container"> {/* i know a br tag is bad since they get evaluated differently by different browsers, but i cant get what i want otherwise*/}
-        <h1 className='calc-title'>Calculator-1000</h1><br />>
+        <h1 className='calc-title'>Calculator-1000</h1><br />
 
     
       <div class="calc-header"></div>
